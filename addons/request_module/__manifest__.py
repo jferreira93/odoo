ab10f26c97fa66cbd8f9ab674b8d879a0af5f659
jferreira93 <jferreira@loopstud.io>
2018-11-12 14:03:03 -0300

    Remove more unused files

--- addons/request_module/__manifest__.py
+++ addons/request_module/__manifest__.py
@@ -28,11 +28,5 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
