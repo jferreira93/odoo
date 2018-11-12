7eda3f72ad963a75846565a0a04a46f1eba93d38
jferreira93 <jferreira@loopstud.io>
2018-11-12 13:54:24 -0300

    Remove more unused files

--- addons/request_module/models/models.py
+++ addons/request_module/models/models.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, api
 import odoo.addons.crm.models.crm_lead as crm_models_crm_lead
 
 import logging
 
 _logger = logging.getLogger(__name__)
-
-class request_module(models.Model):
-    _name = 'request_module.request_module'
-
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
 
 class request_module(crm_models_crm_lead.Lead):
     @api.multi
