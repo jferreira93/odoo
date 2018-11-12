17c7d76c86ca7c33c66dc1537e4cf78bc422ef53
jferreira93 <jferreira@loopstud.io>
2018-11-12 13:35:30 -0300

    Remove unused files and restore default odoo method

--- addons/crm/models/crm_lead.py
+++ addons/crm/models/crm_lead.py
@@ -360,7 +360,6 @@ class Lead(models.Model):
     @api.multi
     def action_set_won(self):
         """ Won semantic: probability = 100 (active untouched) """
-        logging.warning("Mark as won! %s", self)
         for lead in self:
             stage_id = lead._stage_find(domain=[('probability', '=', 100.0), ('on_change', '=', True)])
             lead.write({'stage_id': stage_id.id, 'probability': 100})
