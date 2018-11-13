fcf8a19197f5f3fcde9f0b568d1f3facbc9c917c
jferreira93 <jferreira@loopstud.io>
2018-11-13 11:30:15 -0300

    Implement first model of redmine_request

--- plugins/crm_patch/__manifest__.py
+++ plugins/crm_patch/__manifest__.py
@@ -19,21 +19,9 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Aplicaciones',
+    'category': 'Plugin',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'depends': ['base', 'crm'],
 }
--- plugins/crm_patch/models/crm_patch.py
+++ plugins/crm_patch/models/crm_patch.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
-import logging
+from odoo import api
 import odoo.addons.crm.models.crm_lead as crm_models_crm_lead
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class crm_patch(crm_models_crm_lead.Lead):
-    _name = 'crm_patch.crm_patch'
 
     @api.multi
     def action_set_won(self):
         """ Won semantic: probability = 100 (active untouched) """
-        logging.warning("Mark as won! (plugin) %s", self)
+        logging.warning("Yeeeesss! Mark as won! (plugin) %s", self)
         for lead in self:
             stage_id = lead._stage_find(domain=[('probability', '=', 100.0), ('on_change', '=', True)])
             lead.write({'stage_id': stage_id.id, 'probability': 100})
