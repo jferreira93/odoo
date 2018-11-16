668c45bb0d161cacb7981566447e59fded0e0ed6
jferreira93 <jferreira@loopstud.io>
2018-11-16 10:31:26 -0300

    Remove loggin.warning

--- plugins/crm_patch/models/crm_patch.py
+++ plugins/crm_patch/models/crm_patch.py
@@ -13,21 +13,10 @@ class Lead(models.Model):
     @api.multi
     def action_set_won(self):
         """ Won semantic: probability = 100 (active untouched) """
-        logging.warning("Yeeeesss! Mark as won! (plugin) %s", self)
-        logging.warning("Neeeew plugin implementation!")
+
         for lead in self:
-
             r = self.prepare_new_project_request(lead)
-
-            logging.warning("Request response is %s", r.json())
-            logging.warning("Request status code is %s", r.status_code)
-            logging.warning("Lead name is %s", lead.name)
-            logging.warning("Lead contact_name is %s", lead.contact_name)
-            logging.warning("Lead description is %s", lead.description)
-            logging.warning("Lead planned_revenue is %s", lead.planned_revenue)
-            logging.warning("Lead create_date is %s", lead.create_date)
-            logging.warning("Lead date_action_last is %s", lead.date_action_last)
-
+            r.json()
             stage_id = lead._stage_find(domain=[('probability', '=', 100.0), ('on_change', '=', True)])
             lead.write({'stage_id': stage_id.id, 'probability': 100})
 
@@ -40,7 +29,6 @@ class Lead(models.Model):
 
     def prepare_new_project_request(self, lead):
         identifier = self.create_identifier(lead.name)
-
         url = "http://localhost:3000/projects.json"
         data = { "project": { "name": lead.name, "identifier": identifier } }
         headers = {"Content-type": "application/json", "X-Redmine-API-Key": "ce60ab267dc411a725b0f36a77fe4097dc118542"}
