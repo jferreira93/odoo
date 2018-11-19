3c4aa7f283f761d8bcfac8a112e411360761835b
jferreira93 <jferreira@loopstud.io>
2018-11-19 15:37:10 -0300

    Add links to download in project descriptions

--- plugins/crm_patch/models/crm_patch.py
+++ plugins/crm_patch/models/crm_patch.py
@@ -15,8 +15,24 @@ class Lead(models.Model):
         """ Won semantic: probability = 100 (active untouched) """
 
         for lead in self:
-            r = self.prepare_new_project_request(lead)
+            description = ['See attachments here: ']
+
+            attachments = self.get_lead_attachments(lead.id)
+            for attachment in attachments:
+                logging.warning("Attachment id: %s", attachment.id)
+                name = attachment.name
+                logging.warning("Attachment name: %s", name)
+                file_url = "http://localhost:8069/web/content/{0}/{1}".format(attachment.id, attachment.name)
+                logging.warning("Description temp: %s", file_url)
+                description.append(file_url)
+
+                # self.download_attachment(attachment, file_url)
+
+
+
+            r = self.prepare_new_project_request(lead, description)
             r.json()
+
             stage_id = lead._stage_find(domain=[('probability', '=', 100.0), ('on_change', '=', True)])
             lead.write({'stage_id': stage_id.id, 'probability': 100})
 
@@ -28,11 +44,46 @@ class Lead(models.Model):
 
         return identifier
 
-    def prepare_new_project_request(self, lead):
+    def download_attachment(self, attachment, file_url):
+        logging.warning("Download has to start")
+        return {
+            'type': 'ir.actions.act_url',
+            'url': file_url,
+            'target': 'new'
+        }
+
+    def prepare_new_project_request(self, lead, description):
         identifier = self.create_identifier(lead.name)
         url = "http://localhost:3000/projects.json"
-        data = { "project": { "name": lead.name, "identifier": identifier } }
+        data = { "project": { "name": lead.name, "identifier": identifier, "description": description } }
         headers = {"Content-type": "application/json", "X-Redmine-API-Key": "ce60ab267dc411a725b0f36a77fe4097dc118542"}
         r = requests.post(url, data=json.dumps(data), headers=headers)
 
         return r
+
+    def get_lead_attachments(self, lead_id):
+        attachments = self.env['ir.attachment'].search([('res_model', '=', self._name), ('res_id', '=', lead_id)])
+
+        return attachments
+
+    @api.model
+    def get_location(self):
+        location = self.env['ir.config_parameter'].sudo().get_param('ir_attachment.location', 'file')
+        logging.warning("Location: %s", location)
+
+        return location
+
+    @api.model
+    def _full_path(self, path):
+        # sanitize path
+        path = re.sub('[.]', '', path)
+        path = path.strip('/\\')
+        return os.path.join(self._filestore(), path)
+
+    @api.model
+    def _get_path(self, bin_data, sha):
+        # retro compatibility
+        fname = sha[:3] + '/' + sha
+        full_path = self._full_path(fname)
+        if os.path.isfile(full_path):
+            return fname, full_path        # keep existing path
